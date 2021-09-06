bfbd6f8b35a7eaf60b651a06965a9a56ffaef91b
Trần Quốc Vinh <quocvinhv1212@gmail.com>
2021-09-06 20:22:24 +0700

    test
    test

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
+n = int(input().strip())
+check = {True: "Not Weird", False: "Weird"}
 
-import math 
-import os 
-import random 
-import re 
-import sys
-
-if name == 'main': n = int(raw_input().strip())
-    #print(n % 2)
-    if n%2 != 0: ## odd
-        print("Weird")
-    else: ## even
-        if (n >= 2) and (n <= 5):
-            print("Not Weird")
-        elif n <= 20:
-            print("Weird")
-        else:
-            print("Not Weird")
+print(check[
+        n%2==0 and (
+            n in range(2,6) or 
+            n > 20)
+    ])
